b6c962cefb525415f6c1d6e226204a5427cf444c
Guillem Gari <ggari@robotnik.es>
2023-07-20 15:59:55 +0300

    added first prototype of cpu load

--- cpu_monitor/cpu_monitor.py
+++ cpu_monitor/cpu_monitor.py
@@ -1,7 +1,9 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Temperature
+from std_msgs.msg import String
 from os.path import exists
+import psutil
 
 
 class CPUMonitor(Node):
@@ -14,7 +16,7 @@ class CPUMonitor(Node):
         self.init_parameters()
         self.init_publishers()
         self.init_vars()
-        self.timer = self.create_timer(1, self.publish_temperatures)
+        self.timer = self.create_timer(1, self.publish_cpu_stats)
 
     def init_parameters(self):
         self.cpu_id = "x86_pkg_temp"
@@ -26,19 +28,28 @@ class CPUMonitor(Node):
         #     "publish_rate",
         # ).get_parameter_value().double_value
 
+
+
     def init_publishers(self):
-        self.cpu_output_topic = "cpu_temperature"
-        # self.cpu_output_topic = self.get_parameter(
-        #     "cpu_output_topic"
+        self.cpu_temp_output_topic = "cpu_temperature"
+        self.cpu_load_output_topic = "cpu_load"
+        # self.cpu_temp_output_topic = self.get_parameter(
+        #     "cpu_temp_output_topic"
         # ).get_parameter_value().string_value
-        self.cpu_publisher = self.create_publisher(
+        self.cpu_temp_publisher = self.create_publisher(
             Temperature,
-            self.cpu_output_topic,
+            self.cpu_temp_output_topic,
+            10
+        )
+        self.cpu_load_publisher = self.create_publisher(
+            String,
+            self.cpu_load_output_topic,
             10
         )
 
     def init_vars(self):
         self.cpu_temp_msg = Temperature()
+        self.cpu_load_msg = String()
         self.cpu_temp_msg.header.frame_id = "CPU"
         self.cpu_zone = self.find_cpu_zone()
 
@@ -64,12 +75,15 @@ class CPUMonitor(Node):
             temperature_file.close()
             self.cpu_temp_msg.temperature = float(temperature / 1000)
 
-    def get_gpu_temperature(self):
-        self.gpu_temp_msg.temperature = self.gpu.temperature
+    def get_cpu_load(self):
+        cpu_load = psutil.cpu_percent(interval=0.5)
+        self.cpu_load.string = str(cpu_load)
 
-    def publish_temperatures(self):
+    def publish_cpu_stats(self):
         self.get_cpu_temperature()
-        self.cpu_publisher.publish(self.cpu_temp_msg)
+        self.get_cpu_load()
+        self.cpu_temp_publisher.publish(self.cpu_temp_msg)
+        self.cpu_load_publisher.publish(self.cpu_load)
 
 
 def main(
