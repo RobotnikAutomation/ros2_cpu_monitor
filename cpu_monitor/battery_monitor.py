f5d46f39eafaacc3ad66548cc984083d289b0425
Guillem Gari <ggari@robotnik.es>
2023-10-24 16:06:56 +0200

    removed unsued code on battery

--- cpu_monitor/battery_monitor.py
+++ cpu_monitor/battery_monitor.py
@@ -1,7 +1,5 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Temperature
-# from std_msgs.msg import String
 from robotnik_msgs.msg import BatteryStatus
 from influxdb import InfluxDBClient
 import time
@@ -18,7 +16,6 @@ class BatteryMonitor(Node):
         )
         self.init_parameters()
         self.init_suscribers()
-        self.init_vars()
         self.__setup_influxdb()
 
     def init_parameters(self):
@@ -67,47 +64,6 @@ class BatteryMonitor(Node):
             self.influxdb.close()
         return status
 
-    def init_vars(self):
-        pass
-        # self.cpu_temp_msg = Temperature()
-        # self.cpu_load_msg = String()
-        # self.cpu_temp_msg.header.frame_id = "CPU"
-        # self.cpu_zone = self.find_cpu_zone()
-
-
-    def get_cpu_temperature(self):
-        if self.cpu_zone == -1:
-            return False
-        temperature_file_path = f'/sys/class/thermal/thermal_zone{str(self.cpu_zone)}/temp'
-        temperature_file = open(temperature_file_path, "r")
-        temperature = int(temperature_file.read().strip())
-        temperature_file.close()
-        now = time.time_ns()
-        self.cpu_temp_msg.temperature = float(temperature / 1000)
-        if not self.influxdb_health:
-            return False
-        data = {
-            "measurement": "cpu",
-            "tags": {
-                "unit": "Celsius",
-            },
-            "time": now,
-            "fields": {
-                "temperature": self.cpu_temp_msg.temperature,
-            }
-        }
-        json_payload = []
-        data['fields']['size'] = sys.getsizeof(str(data))
-        json_payload.append(data)
-        try:
-            self.influxdb.write_points(json_payload)
-        except Exception as e:
-            self.get_logger().error(
-                f"Error writing latency data to InfluxDB: {e}"
-            )
-        return True
-
-
     def battery_callback(self, msg):
         # self.get_logger().info('I heard: "%s"' % msg.voltage)
         now = time.time_ns()
@@ -148,4 +104,3 @@ def main(
     while rclpy.ok():
         rclpy.spin(tracker)
     rclpy.shutdown()
-
